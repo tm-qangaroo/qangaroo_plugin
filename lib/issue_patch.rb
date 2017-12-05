953dfb04a9479062efb81c7c973cc2bc0aa2ace2
d.rhie <d.rhie@tcmobile.jp>
2017-12-05 09:59:05 +0900

    Add update extension to plugin

--- lib/issue_patch.rb
+++ lib/issue_patch.rb
@@ -13,6 +13,27 @@ module Qangaroo
         alias_attribute :projectId, :project_id
         alias_attribute :summary, :subject
         alias_attribute :dueDate, :due_date
+        after_update :update_qangaroo_issue
+
+        def update_qangaroo_issue
+          if self.qangaroo_issue.present?
+            begin
+              results = {self.qangaroo_issue.id => self.qangaroo_issue.issue}
+              service = self.qangaroo_issue.qangaroo_service
+              if service.active
+                uri = URI.parse("http://#{service.namespace}/api/v1/redmine_services/update_issue")
+                http = Net::HTTP.new(uri.host, uri.port)
+
+                request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
+                request.body = results.to_json
+                request.add_field("X-Qangaroo-API-Key", service.api_key)
+                
+                res = http.request(request)
+              end
+            rescue
+            end
+          end
+        end
       end
     end
 
